f7854a489e786163e48e383aa9533b1cfd78a264
raz <unknown>
2003-06-23 11:41:03 +0000

    an example similar with examples/example.c and examples.hs/example but using Omega (not Omega_stub) module

--- examples.hs/myexample/Main.hs
+++ examples.hs/myexample/Main.hs
@@ -12,42 +12,33 @@ import Foreign.C
 import qualified Omega_stub
 
 main = do
-    print "[START relation_new2]"
-    s <- Omega_stub.relation_new1 2
-    print "[START newCString]"
-    n_str <- newCString "n"
-    print "[START free_var_decl0]"
-    n_ptr <- Omega_stub.free_var_decl0 n_str
-    print "[START relation_get_local_global1]"
-    s_local_n_ptr <- Omega_stub.relation_get_local_global1 s n_ptr
-    print "[START let...]"
-    let n = ("n", s_local_n_ptr)
-	sf = RFormula (\x -> RFormula (\y -> (Formula (And [
-							    Geq [Coef x 1, Const (-1)],
-							    Geq [Coef x (-1), Coef n 1],
-							    Geq [Coef x 1, Coef y (-1), Const 5],
-							    Stride 17 [Coef x 1],
-							    Exists (\z -> And [
-									       Geq [Coef z 1, Coef y (-1)],
-									       Geq [Coef x 1, Coef z (-1)],
-									       Or [Stride 8 [Coef z 1], Stride 12 [Coef x 5]]
-									      ]
-								   )
-							   ]
+    print "[START]"
+    let	sf = RFormula (\x -> RFormula (\y -> RFormula (\n -> Formula (And [
+									   Geq [Coef x 1, Const (-1)],
+									   Geq [Coef x (-1), Coef n 1],
+									   Geq [Coef x 1, Coef y (-1), Const 5],
+									   Stride 17 [Coef x 1],
+									   Exists (\z -> And [
+											      Geq [Coef z 1, Coef y (-1)],
+											      Geq [Coef x 1, Coef z (-1)],
+											      Or [Stride 8 [Coef z 1], Stride 12 [Coef x 5]]
+											     ]
+										  )
+									  ]
+								     )
 						      )
-					     )
 				      )
 		      )
-        app_rformula x (RFormula f) = f x
-    print "[START relation_set_var]"
-    x_ptr <- Omega_stub.relation_set_var s 1
-    y_ptr <- Omega_stub.relation_set_var s 2
-    print "[START app_rformula]"
-    let sf1 = app_rformula ("x", x_ptr) sf
-	sf2 = app_rformula ("y", y_ptr) sf1
-    print "[OK putStr]"
-    putStr (show sf2)
---    build_relation (["x", "y"], 2, s)
+    (s, (RFormula sf)) <- build_relation (["x", "y"], 0, sf)
+
+    n_str <- newCString "n"
+    n_ptr <- Omega_stub.free_var_decl0 n_str
+    s_local_n_ptr <- Omega_stub.relation_get_local_global1 s n_ptr
+    let n = ("n", s_local_n_ptr)
+    print (show (sf n))
+    eval_relation s (sf n)
+    Omega_stub.relation_finalize s
+    Omega_stub.relation_print s
     print "[DONE]"
 
 
