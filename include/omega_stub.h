0c6578c6292d202a9f8a22ab95e73004b848e514
raz <unknown>
2003-07-04 03:50:19 +0000

    add new prototipes for more printing, simplification and satisfiability, upper and lower bounds, binary relational operators, unary relation operations, hull operation and relational function that return boolean values

--- include/omega_stub.h
+++ include/omega_stub.h
@@ -1,5 +1,5 @@
 /*
- * $Id: omega_stub.h,v 1.4 2003-06-23 09:57:07 raz Exp $
+ * $Id: omega_stub.h,v 1.5 2003-07-04 03:50:19 raz Exp $
  */
 
 #ifndef _OMEGA_STUB_H
@@ -71,17 +71,82 @@ extern "C" {
   Variable_ID f_forall_declare(F_Forall* f, char* name);
   Variable_ID f_exists_declare(F_Exists* f, char* name);
 
+  Free_Var_Decl* free_var_decl0(char* name);
+  Free_Var_Decl* free_var_decl1(char* name, int arity);
+
   void constraint_handler_update_const(Constraint_Handle* ch, coef_t delta);
   void constraint_handler_update_coef(Constraint_Handle* ch, Variable_ID v, coef_t delta);
   
+  // printing
   void relation_print(Relation* r);
-  void relation_print_file(Relation* r, FILE* output_file);
-  void relation_print_with_subs(Relation* r, FILE* output_file, bool printSym);
-  char* relation_print_with_subs_to_string(Relation* r, bool printSym);
-  char* relation_print_formula_to_string(Relation* r);
+  void relation_print_to_file(Relation* r, FILE* output_file);
+  void relation_print_with_subs_to_file(Relation* r, FILE* output_file, bool printSym);
+  const char* relation_print_with_subs_to_string(Relation* r, bool printSym);
+  const char* relation_print_formula_to_string(Relation* r);
 
-  Free_Var_Decl* free_var_decl0(char* name);
-  Free_Var_Decl* free_var_decl1(char* name, int arity);
+  // simplification and satisfiability
+  bool is_upper_bound_satistiable(Relation* r);
+  bool is_lower_bound_satisfiable(Relation* r);
+  bool is_satisfiable(Relation* r);
+  bool is_obvious_tautology(Relation* r);
+  bool is_definite_tautology(Relation* r);
+  bool is_exact(Relation* r);
+  bool is_inexact(Relation* r);
+  bool is_unknown(Relation* r);
+
+  // upper and lower bounds
+  Relation* upper_bound(Relation* r);
+  Relation* lower_bound(Relation* r);
+  
+  // binary relational operators
+  Relation* union_relation(Relation *r1, Relation *r2);
+  Relation* intersection(Relation *r1, Relation *r2);
+  Relation* composition(Relation *r1, Relation *r2);
+  Relation* join(Relation *r1, Relation *r2);
+  Relation* restrict_domain(Relation *r1, Relation *r2);
+  Relation* restrict_range(Relation *r1, Relation *r2);
+  Relation* difference(Relation *r1, Relation *r2);
+  Relation* cross_product(Relation *r1, Relation *r2);
+  Relation* gist(Relation *r1, Relation *r2);
+
+  // unary relation operations
+  Relation* transitive_closure1(Relation *r);
+  Relation* transitive_closure2(Relation *r1, Relation *r2);
+  Relation* domain(Relation *r);
+  Relation* range(Relation *r);
+  Relation* inverse(Relation *r);
+  Relation* complement(Relation *r);
+  Relation* project(Relation *r, Global_Var_ID v);
+  Relation* project_sym(Relation *r);
+  Relation* project_on_sym(Relation *r);
+  Relation* extend_domain1(Relation *r);
+  Relation* extend_domain2(Relation *r, int n);
+  Relation* range_domain1(Relation *r);
+  Relation* range_domain2(Relation *r, int n);
+  Relation* set(Relation *r);
+  Relation* extend_set(Relation *r, int n);
+  Relation* daltas1(Relation *r);
+  Relation* daltas2(Relation *r, int n);
+  Relation* approximate1(Relation *r);
+  Relation* approximate2(Relation *r, int n);
+  Relation* egs_to_geqs1(Relation *r);
+  Relation* egs_to_geqs2(Relation *r, bool flag);
+  Relation* sample_solution(Relation *r);
+  Relation* symbolic_solution(Relation *r);
+  
+  // advance operations
+  Relation* convex_hull(Relation* r);
+  Relation* decoupled_convex_hull(Relation* r);
+  Relation* affine_hull(Relation* r);
+  Relation* linear_hull(Relation* r);
+  Relation* fast_tight_hull(Relation* r);
+  Relation* fast_tight_hull(Relation* r);
+  
+  // relational function that return boolean values
+  bool must_be_subset(Relation* r1, Relation* r2);
+  bool might_be_subset(Relation* r1, Relation* r2);
+  bool is_obvious_subset(Relation* r1, Relation* r2);
+
 }
 
 #endif
