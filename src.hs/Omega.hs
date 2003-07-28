f4ca805fc3447cdfc5028e8c2dbb603d72dd089a
raz <unknown>
2003-07-28 05:09:35 +0000

    remove types (moved to Omega_types); remove previous attempt to explore a Omega formula using iterators

--- src.hs/Omega.hs
+++ src.hs/Omega.hs
@@ -11,210 +11,12 @@ module Omega where
 
 import Foreign
 import Foreign.C
-import qualified Omega_stub
 import Omega_util
+import Omega_types
+import qualified Omega_stub
+import Omega_parser
 import Prelude hiding ((>),(&&),(||))
-
-type Relation = ([Variable_name], [Variable_name], RFormula)
-
-data RFormula = RFormula (Variable -> RFormula)
-	      | Formula Formula
-
-data Formula = And [Formula]
-	     | Or [Formula]
-	     | Not Formula
-	     | Exists (Variable -> Formula)
-	     | Forall (Variable -> Formula)
-	     | Geq [Update]
-	     | Eq [Update]
-	     | Stride Int [Update]
-
-data Update = Coef Variable Int
-	    | Const Int
-
-type Variable = (Variable_name, Ptr Omega_stub.Variable)
-
-type Variable_name = String
-
-instance Show Update where
-    show (Const i) = show i
-    show (Coef v i) = show i ++ "*" ++ show v
-
-instance Show Formula where
-    show (And c) = let show_vec :: [Formula] -> String
-		       show_vec [] = show "--void--"
-		       show_vec [c] = show c
-		       show_vec (c:cs) = show c ++ " && " ++ show_vec cs
-		   in "(" ++ show_vec c ++ ")"
-    show (Or c) = let show_vec :: [Formula] -> String
-		      show_vec [c] = show c
-		      show_vec (c:cs) = show c ++ " || " ++ show_vec cs
-		  in "(" ++ show_vec c ++ ")"
-    show (Not c) = "(! " ++ show c ++ ")"
-    show (Exists f) = "\\exists _ . " ++ show (f ("_", nullPtr))
-    show (Forall f) = "\\forall _ . " ++ show (f ("_", nullPtr))
-
-    show (Geq u) = let show_vec :: [Update] -> String
-		       show_vec [u] = show u
-		       show_vec (u:us) = show u ++ " + " ++ show_vec us
-		   in show_vec u ++ " >= 0"
-    show (Eq u) = let show_vec :: [Update] -> String
-		      show_vec [] = "--void--"
-		      show_vec [u] = show u
-		      show_vec (u:us) = show u ++ " + " ++ show_vec us
-		  in show_vec u ++ " = 0"
-    show (Stride i u) = let show_vec :: [Update] -> String
-		            show_vec [u] = show u
-		            show_vec (u:us) = show u ++ " + " ++ show_vec us
-			in show i ++ "|" ++ show_vec u
-
-instance Show RFormula where
-    show (Formula f) = "(F) . " ++ show f
-    show (RFormula r) = "[*] . " ++ show (r ("[*]", nullPtr))
-
---instance Show Variable where
---    show (var_name, var_ptr) = show var_name
-
-eval_RFormula :: [String] -> RFormula -> IO String
-eval_RFormula _ (Formula f) = do let r = show f
-				 putStr r
-				 return r
-eval_RFormula (var:vars) (RFormula rf) = eval_RFormula vars (rf (var, nullPtr))
-
--- experimental stuff
-
-class Evaluable r where
-    add_and :: (Ptr r) -> IO (Ptr Omega_stub.F_And)
-    add_or :: (Ptr r) -> IO (Ptr Omega_stub.F_Or)
-    add_not :: (Ptr r) -> IO (Ptr Omega_stub.F_Not)
-    add_forall :: (Ptr r) -> IO (Ptr Omega_stub.F_Forall)
-    add_exists :: (Ptr r) -> IO (Ptr Omega_stub.F_Exists)
-    add_geq :: (Ptr r) -> IO (Ptr Omega_stub.GEQ_Handle)
-    add_eq :: (Ptr r) -> IO (Ptr Omega_stub.EQ_Handle)
-    add_stride :: (Ptr r) -> CInt -> IO (Ptr Omega_stub.Stride_Handle)
-
-instance Evaluable Omega_stub.Relation where
-    add_and r = do debug_msg ("relation_add_and " ++ (show r) ++ "\n")
-		   Omega_stub.relation_add_and r
-    add_or  r = do debug_msg ("relation_add_or " ++ (show r) ++ "\n")
-		   Omega_stub.relation_add_or r
-    add_not r = do debug_msg ("relation_add_or " ++ (show r) ++ "\n")
-		   Omega_stub.relation_add_not r
-    add_forall r = do debug_msg ("relation_add_forall " ++ (show r) ++ "\n")
-		      Omega_stub.relation_add_forall r
-    add_exists r = do debug_msg ("relation_add_exists " ++ (show r) ++ "\n")
-		      Omega_stub.relation_add_exists r
-    add_geq r = do debug_msg ("relation_add_geq " ++ (show r) ++ "\n")
-		   f <- Omega_stub.relation_add_and r
-		   Omega_stub.f_and_add_GEQ f
-    add_eq r = do debug_msg ("relation_add_eq " ++ (show r) ++ "\n")
-		  f <- Omega_stub.relation_add_and r
-		  Omega_stub.f_and_add_EQ f
-    add_stride r n = do debug_msg ("relation_add_stride " ++ (show r) ++ "\n")
-			f <- Omega_stub.relation_add_and r
-			Omega_stub.f_and_add_stride f n
-
-instance Evaluable Omega_stub.F_And where
-    add_and f = do debug_msg ("f_and_add_and " ++ (show f) ++ "\n")
-		   Omega_stub.f_and_add_and f
-    add_or  f = do debug_msg ("f_and_add_or " ++ (show f) ++ "\n")
-		   Omega_stub.f_and_add_or f
-    add_not f = do debug_msg ("f_and_add_not " ++ (show f) ++ "\n")
-		   Omega_stub.f_and_add_not f
-    add_forall f = do debug_msg ("f_and_add_forall " ++ (show f) ++ "\n")
-		      Omega_stub.f_and_add_forall f
-    add_exists f = do debug_msg ("f_and_add_exists " ++ (show f) ++ "\n")
-		      Omega_stub.f_and_add_exists f
-    add_geq f = do debug_msg ("f_and_add_geq " ++ (show f) ++ "\n")
-		   Omega_stub.f_and_add_GEQ f
-    add_eq f = do debug_msg ("f_and_add_eq " ++ (show f) ++ "\n")
-		  Omega_stub.f_and_add_EQ f
-    add_stride f n = do debug_msg ("f_and_add_stride " ++ (show f) ++ "\n")
-			Omega_stub.f_and_add_stride f n
-
-instance Evaluable Omega_stub.F_Or where
-    add_and f = do debug_msg ("f_or_add_and " ++ (show f) ++ "\n")
-		   Omega_stub.f_or_add_and f
-    add_or  f = do debug_msg ("f_or_add_or " ++ (show f) ++ "\n")
-		   Omega_stub.f_or_add_or f
-    add_not f = do debug_msg ("f_or_add_not " ++ (show f) ++ "\n")
-		   Omega_stub.f_or_add_not f
-    add_forall f = do debug_msg ("f_or_add_forall " ++ (show f) ++ "\n")
-		      Omega_stub.f_or_add_forall f
-    add_exists f = do debug_msg ("f_or_add_exists " ++ (show f) ++ "\n")
-		      Omega_stub.f_or_add_exists f
-    add_geq f = do debug_msg ("f_or_add_geq " ++ (show f) ++ "\n")
-		   f' <- Omega_stub.f_or_add_and f
-		   Omega_stub.f_and_add_GEQ f'
-    add_eq f = do debug_msg ("f_or_add_eq " ++ (show f) ++ "\n")
-		  f' <- Omega_stub.f_or_add_and f
-		  Omega_stub.f_and_add_EQ f'
-    add_stride f n = do debug_msg ("f_or_add_stride " ++ (show f) ++ "\n")
-			f' <- Omega_stub.f_or_add_and f
-			Omega_stub.f_and_add_stride f' n
-
-instance Evaluable Omega_stub.F_Not where
-    add_and f = do debug_msg ("f_not_add_and " ++ (show f) ++ "\n")
-		   Omega_stub.f_not_add_and f
-    add_or  f = do debug_msg ("f_not_add_or " ++ (show f) ++ "\n")
-		   Omega_stub.f_not_add_or f
-    add_not f = do debug_msg ("f_not_add_not " ++ (show f) ++ "\n")
-		   Omega_stub.f_not_add_not f
-    add_forall f = do debug_msg ("f_not_add_forall " ++ (show f) ++ "\n")
-		      Omega_stub.f_not_add_forall f
-    add_exists f = do debug_msg ("f_not_add_exists " ++ (show f) ++ "\n")
-		      Omega_stub.f_not_add_exists f
-    add_geq f = do debug_msg ("f_not_add_geq " ++ (show f) ++ "\n")
-		   f' <- Omega_stub.f_not_add_and f
-		   Omega_stub.f_and_add_GEQ f'
-    add_eq f = do debug_msg ("f_not_add_eq " ++ (show f) ++ "\n")
-		  f' <- Omega_stub.f_not_add_and f
-		  Omega_stub.f_and_add_EQ f'
-    add_stride f n = do debug_msg ("f_not_add_stride " ++ (show f) ++ "\n")
-			f' <- Omega_stub.f_not_add_and f
-			Omega_stub.f_and_add_stride f' n
-
-instance Evaluable Omega_stub.F_Forall where
-    add_and f = do debug_msg ("f_forall_add_and " ++ (show f) ++ "\n")
-		   Omega_stub.f_forall_add_and f
-    add_or  f = do debug_msg ("f_forall_add_or " ++ (show f) ++ "\n")
-		   Omega_stub.f_forall_add_or f
-    add_not f = do debug_msg ("f_forall_add_forall " ++ (show f) ++ "\n")
-		   Omega_stub.f_forall_add_not f
-    add_forall f = do debug_msg ("f_forall_add_forall " ++ (show f) ++ "\n")
-		      Omega_stub.f_forall_add_forall f
-    add_exists f = do debug_msg ("f_forall_add_exists " ++ (show f) ++ "\n")
-		      Omega_stub.f_forall_add_exists f
-    add_geq f = do debug_msg ("f_forall_add_geq " ++ (show f) ++ "\n")
-		   f' <- Omega_stub.f_forall_add_and f
-		   Omega_stub.f_and_add_GEQ f'
-    add_eq f = do debug_msg ("f_forall_add_eq " ++ (show f) ++ "\n")
-		  f' <- Omega_stub.f_forall_add_and f
-		  Omega_stub.f_and_add_EQ f'
-    add_stride f n = do debug_msg ("f_forall_add_stride " ++ (show f) ++ "\n")
-			f' <- Omega_stub.f_forall_add_and f
-			Omega_stub.f_and_add_stride f' n
-
-instance Evaluable Omega_stub.F_Exists where
-    add_and f = do debug_msg ("f_exists_add_and " ++ (show f) ++ "\n")
-		   Omega_stub.f_exists_add_and f
-    add_or  f = do debug_msg ("f_exists_add_or " ++ (show f) ++ "\n")
-		   Omega_stub.f_exists_add_or f
-    add_not f = do debug_msg ("f_exists_add_forall " ++ (show f) ++ "\n")
-		   Omega_stub.f_exists_add_not f
-    add_forall f = do debug_msg ("f_exists_add_forall " ++ (show f) ++ "\n")
-		      Omega_stub.f_exists_add_forall f
-    add_exists f = do debug_msg ("f_exists_add_exists " ++ (show f) ++ "\n")
-		      Omega_stub.f_exists_add_exists f
-    add_geq f = do debug_msg ("f_exists_add_geq " ++ (show f) ++ "\n")
-		   f' <- Omega_stub.f_exists_add_and f
-		   Omega_stub.f_and_add_GEQ f'
-    add_eq f = do debug_msg ("f_exists_add_eq " ++ (show f) ++ "\n")
-		  f' <- Omega_stub.f_exists_add_and f
-		  Omega_stub.f_and_add_EQ f'
-    add_stride f n = do debug_msg ("f_exists_add_stride " ++ (show f) ++ "\n")
-			f' <- Omega_stub.f_exists_add_and f
-			Omega_stub.f_and_add_stride f' n
+import List
 
 
 build_relation :: Relation -> IO ((Ptr Omega_stub.Relation), RFormula)
@@ -368,12 +170,6 @@ instance Arith_plus Variable Variable where
     v1 `minus` v2 = [Coef v1 1, Coef v2 (- 1)]
 
 
-minus_update :: [Update] -> [Update]
-minus_update [] = []
-minus_update ((Const i):us) = (Const (- i)):(minus_update us)
-minus_update ((Coef v i):us) = (Coef v (- i)):(minus_update us)
-
-
 instance Arith_q [Update] Int where
     us `eq` 0 = Eq us
     us `eq` i = Eq ((Const (- i)):us)
@@ -420,61 +216,66 @@ instance Arith_mul Variable Int where
 n `div` us = Stride n us
 
 
-extract_formula_from_set :: (Ptr Omega_stub.Relation) -> (IO RFormula)
-extract_formula_from_set ptr_r =
-    do Omega_stub.relation_finalize ptr_r
-       vars_no <- Omega_stub.relation_n_set ptr_r
-       debug_msg ("HS_DEBUG(extract_formula_from_set): vars_no: " ++ (show vars_no) ++ "\n")
+--relation_extract_rformula :: (Ptr Omega_stub.Relation) -> IO RFormula
+--relation_extract_rformula r =
+--    do 
 
-       let vars_to_eq [] = []
-           vars_to_eq (v:vars) = (Coef v 1: (vars_to_eq vars))
-	   build_formula _ 0 vars = Formula (And [Eq (vars_to_eq vars)])
-           build_formula vars_no i vars = RFormula (\x -> (build_formula vars_no (i - 1) (x:vars)))
-
-       return (build_formula vars_no vars_no [])
---       return (Formula (And []))
-
---     Omega_stub.relation_setup_names ptr_r3
---     let while_1 :: (Ptr Omega_stub.DNF_Iterator) -> IO ()
---         while_1 ptr_dnf_iterator =
--- 	    do let while_2 :: (Ptr Omega_stub.EQ_Iterator) -> IO ()
--- 		   while_2 ptr_eq_iterator =
--- 		       do let while_3 :: (Ptr Omega_stub.Constr_Vars_Iter) -> IO ()
--- 			      while_3 ptr_constr_iter =
--- 				  do debug_msg "HS_DEBUG: while_3\n"
--- 				     more <- Omega_stub.constr_iter_more ptr_constr_iter
--- 				     if (not more)
--- 					then do debug_msg "HS_DEBUG: while_3 (done)\n"
--- 					        return ()
--- 					else do coef <- Omega_stub.constr_iter_get_coef ptr_constr_iter
--- 						ptr_var <- Omega_stub.constr_iter_get_variable ptr_constr_iter
--- 						kind <- Omega_stub.variable_kind ptr_var
--- 						pos <- Omega_stub.variable_get_position ptr_var
--- 						ptr_name <- Omega_stub.variable_name ptr_var
--- 						name <- peekCString ptr_name
--- 						putStr ("Coef: " ++ name ++ "(" ++ (show pos) ++ ")/" ++ (show kind) ++ " * " ++ (show coef) ++ "\n")
--- 						Omega_stub.constr_iter_next ptr_constr_iter
--- 						while_3 ptr_constr_iter
---                           debug_msg "HS_DEBUG: while_2\n"
--- 			  more <- Omega_stub.eq_iterator_more ptr_eq_iterator
--- 			  if (not more)
--- 			     then do debug_msg "HS_DEBUG: while_2 (done)\n"
--- 				     return ()
--- 			     else do ptr_constr_iter <- Omega_stub.eq_constr_iter_new ptr_eq_iterator
--- 				     while_3 ptr_constr_iter
--- 				     const <- Omega_stub.eq_get_const ptr_eq_iterator
--- 				     putStr ("Const: " ++ (show const) ++ "\n")
--- 				     Omega_stub.eq_iterator_next ptr_eq_iterator
--- 				     while_2 ptr_eq_iterator
---                debug_msg "HS_DEBUG: while_1\n"
--- 	       more <- Omega_stub.dnf_iterator_more ptr_dnf_iterator
--- 	       if (not more)
--- 		  then do debug_msg "HS_DEBUG: while_1 (done)\n"
--- 			  return ()
--- 		  else do ptr_eq_iterator <- Omega_stub.eq_iterator_new ptr_dnf_iterator
--- 			  while_2 ptr_eq_iterator
--- 			  Omega_stub.dnf_iterator_next ptr_dnf_iterator
--- 			  while_1 ptr_dnf_iterator
---     ptr_dnf_iterator_r3 <- Omega_stub.dnf_iterator_new1 ptr_r3
---     while_1 ptr_dnf_iterator_r3
+-- extract_formula_from_set :: (Ptr Omega_stub.Relation) -> (IO RFormula)
+-- extract_formula_from_set ptr_r =
+--     do let vars_to_eq [] = []
+--            vars_to_eq (v:vars) = (Coef v 1: (vars_to_eq vars))
+--            formula_building vars = 
+-- 	       do Omega_stub.relation_setup_names ptr_r
+-- 		  let while_1 :: (Ptr Omega_stub.DNF_Iterator) -> IO ()
+-- 		      while_1 ptr_dnf_iterator =
+--  		          do let while_2 :: (Ptr Omega_stub.EQ_Iterator) -> IO ()
+--  				 while_2 ptr_eq_iterator =
+--  				     do let while_3 :: (Ptr Omega_stub.Constr_Vars_Iter) -> IO ()
+--  					    while_3 ptr_constr_iter =
+--  						do debug_msg "HS_DEBUG(extract_formula_from_set): while_3\n"
+--  						   more <- Omega_stub.constr_iter_more ptr_constr_iter
+--  						   if (not more)
+--  						      then do debug_msg "HS_DEBUG(extract_formula_from_set): while_3 (done)\n"
+--  							      return []
+--  						      else do coef <- Omega_stub.constr_iter_get_coef ptr_constr_iter
+--  							      ptr_var <- Omega_stub.constr_iter_get_variable ptr_constr_iter
+--  							      kind <- Omega_stub.variable_kind ptr_var
+--  							      pos <- Omega_stub.variable_get_position ptr_var
+-- -- 						                ptr_name <- Omega_stub.variable_name ptr_var
+-- -- 						                name <- peekCString ptr_name
+-- -- 						                putStr ("Coef: " ++ name ++ "(" ++ (show pos) ++ ")/" ++ (show kind) ++ " * " ++ (show coef) ++ "\n")
+--  							      Omega_stub.constr_iter_next ptr_constr_iter
+--  							      return ((Coef (genericIndex vars pos) coef):(while_3 ptr_constr_iter))
+--                                         debug_msg "HS_DEBUG(extract_formula_from_set): while_2\n"
+--  					more <- Omega_stub.eq_iterator_more ptr_eq_iterator
+--  					if (not more)
+--  					   then do debug_msg "HS_DEBUG(extract_formula_from_set): while_2 (done)\n"
+--  						   return []
+--  					   else do ptr_constr_iter <- Omega_stub.eq_constr_iter_new ptr_eq_iterator
+--  						   while_3 ptr_constr_iter
+--  						   const <- Omega_stub.eq_get_const ptr_eq_iterator
+--  						   putStr ("Const: " ++ (show const) ++ "\n")
+--  						   Omega_stub.eq_iterator_next ptr_eq_iterator
+--  						   return Eq ((while_2 ptr_eq_iterator):(Const const))
+--                              debug_msg "HS_DEBUG(extract_formula_from_set): while_1\n"
+--  			     more <- Omega_stub.dnf_iterator_more ptr_dnf_iterator
+--  			     if (not more)
+--  				then do debug_msg "HS_DEBUG(extract_formula_from_set): while_1 (done)\n"
+--  					return []
+--  				else do ptr_eq_iterator <- Omega_stub.eq_iterator_new ptr_dnf_iterator
+--  					eq <- while_2 ptr_eq_iterator
+--  					Omega_stub.dnf_iterator_next ptr_dnf_iterator
+--  					return eq:(while_1 ptr_dnf_iterator)
+--                   ptr_dnf_iterator_r <- Omega_stub.dnf_iterator_new1 ptr_r
+-- 		  while_1 ptr_dnf_iterator_r
+-- --	   build_formula _ 0 vars = Formula (And [Eq (vars_to_eq vars)])
+-- 	   build_formula _ 0 vars = do vec_f <- formula_building vars
+-- 				       return Formula (And vec_f)
+--            build_formula vars_no i vars = do rf <- build_formula vars_no (i - 1) (x:vars)
+-- 					     return RFormula (\x -> rf)
+--        Omega_stub.relation_finalize ptr_r
+--        vars_no <- Omega_stub.relation_n_set ptr_r
+--        debug_msg ("HS_DEBUG(extract_formula_from_set): vars_no: " ++ (show vars_no) ++ "\n")
+--        build_formula vars_no vars_no []
+-- --       return (Formula (And []))
 
