30ee0c12d4daf7a54b2920794aba98317fe54e66
raz <unknown>
2003-07-28 09:37:29 +0000

    testing some parser settings

--- examples.hs/example2/Main.hs
+++ examples.hs/example2/Main.hs
@@ -1,7 +1,7 @@
 ---
 -- This is an example using the new Omega module.
 --
--- $Id: Main.hs,v 1.1 2003-07-28 05:11:22 raz Exp $
+-- $Id: Main.hs,v 1.2 2003-07-28 09:37:29 raz Exp $
 --
 
 module Main(main) where
@@ -12,6 +12,8 @@ import Foreign
 import Foreign.C
 import qualified Omega_stub
 import Omega_util
+import Omega_parser
+import Omega_lexer
 
 main = do
     putStr "[START]\n"
@@ -19,7 +21,7 @@ main = do
     let	rf = RFormula (\i -> RFormula (\j -> RFormula (\k -> Formula (And [ j `eq` (i `plus` k) ] ))))
 --    let	rf3 = RFormula (\i -> RFormula (\j ->  Formula (And [ j `eq` (i `plus` (2::Int))) ] )))
 --    let	rf3 = RFormula (\i -> RFormula (\j -> Formula (And [ Exists (\k -> And [ j `eq` (i `plus` k `plus` (2::Int)) ] ) ] )))
-    (ptr_r, rf) <- build_relation (["a", "b", "c"], [], rf)
+    (ptr_r, rf) <- build_relation (["a", "b"], ["c"], rf)
     putStr ((show rf) ++ "\n")
     eval_relation ptr_r rf
 
@@ -30,13 +32,17 @@ main = do
     Omega_stub.relation_print_with_subs ptr_r True
 --    Omega_stub.query_experiment ptr_r
 
-    ptr_str_print <- Omega_stub.relation_print_formula_to_string ptr_r
-    str_print <- peekCString ptr_str_print
-    putStr ("str_print_formula: " ++ str_print ++ "\n")
+    ptr_str_print_formula <- Omega_stub.relation_print_formula_to_string ptr_r
+    str_print_formula <- peekCString ptr_str_print_formula
+    putStr ("str_print_formula: " ++ str_print_formula ++ "\n")
 
     ptr_str_print_with_subs <- Omega_stub.relation_print_with_subs_to_string ptr_r True
     str_print_with_subs <- peekCString ptr_str_print_with_subs 
     putStr ("relation_print_with_subs: " ++ str_print_with_subs ++ "\n")
+
+    ptr_str_print <- Omega_stub.relation_print_to_string ptr_r
+    str_print <- peekCString ptr_str_print
+    putStr ("relation_print: " ++ str_print ++ "\n")
 
 --    ptr_str_print_outputs <- Omega_stub.relation_print_outputs_with_subs_to_string ptr_r
 --    str_print_outputs <- peekCString ptr_str_print_outputs
@@ -44,6 +50,17 @@ main = do
 
 --    rf' <- extract_formula_from_set ptr_r
 --    putStr ((show rf') ++ "\n")
+
+
+    rf' <- relation_extract_rformula ptr_r
+    putStr ("relation_extract_rformula: " ++ (show rf') ++ "\n")
+
+
+    putStr ((show (extract_rformula "{[a,b] -> [c] a+c = b }")) ++ "\n")
+    putStr ((show (extract_rformula "{[a,b] -> [c] a+c >= b }")) ++ "\n")
+    putStr ((show (extract_rformula "{[a,b] -> [c] a+c <= b }")) ++ "\n")
+
+    putStr ((show (omega_lexer "{[a,b] -> [c] a+c = b }")) ++ "\n")
 
     putStr "[DONE]\n"
 
