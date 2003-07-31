7fa1010efff435ecbfe634c6f1d3269366b9255d
raz <unknown>
2003-07-31 11:18:39 +0000

    '_' is a valid variable name; add &, |, and, or

--- src.hs/Omega_lexer.hs
+++ src.hs/Omega_lexer.hs
@@ -8,6 +8,7 @@ omega_lexer [] = []
 omega_lexer (c:cs) 
 	| isSpace c = omega_lexer cs
 	| isAlpha c = omega_lexVar (c:cs)
+	| c == '_'  = omega_lexVar (c:cs)
 	| isDigit c = omega_lexNum (c:cs)
 omega_lexer ('-':'>':cs) = TokenArrow : omega_lexer cs
 omega_lexer ('=':cs) = TokenEq : omega_lexer cs
@@ -29,17 +30,23 @@ omega_lexer (',':cs) = TokenComma : omega_lexer cs
 omega_lexer (':':cs) = TokenSColon : omega_lexer cs
 omega_lexer ('&':'&':cs) = TokenAnd : omega_lexer cs
 omega_lexer ('|':'|':cs) = TokenOr : omega_lexer cs
+omega_lexer ('&':cs) = TokenAnd : omega_lexer cs
+omega_lexer ('|':cs) = TokenOr : omega_lexer cs
+
+
 
 omega_lexNum cs = TokenInt (read num) : omega_lexer rest
 	where (num,rest) = span isDigit cs
 
-isVarChar c = (isAlphaNum c) || (c == '\'')
+isVarChar c = (isAlphaNum c) || (c == '\'') || (c == '_')
 
 omega_lexVar cs =
    case span isVarChar cs of
-	("exists",rest) -> TokenExists : omega_lexer rest
-	("forall",rest) -> TokenForall : omega_lexer rest
-	("union",rest)  -> TokenUnion : omega_lexer rest
+	("exists",rest) -> TokenExists  : omega_lexer rest
+	("forall",rest) -> TokenForall  : omega_lexer rest
+	("union",rest)  -> TokenUnion   : omega_lexer rest
+	("or",rest)     -> TokenOr      : omega_lexer rest
+	("and",rest)    -> TokenAnd     : omega_lexer rest
 	(var,rest)      -> TokenVar var : omega_lexer rest
 
 -- {[x] -> [y] y = 1+x }
