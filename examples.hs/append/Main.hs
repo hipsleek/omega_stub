3454effd276f4a424261c4c6210fa8b432d14e77
raz <unknown>
2003-08-07 12:42:26 +0000

    tod-down without output vars

--- examples.hs/append/Main.hs
+++ examples.hs/append/Main.hs
@@ -1,7 +1,7 @@
 ---
 -- Fix point for "append"
 --
--- $Id: Main.hs,v 1.6 2003-08-07 04:03:00 raz Exp $
+-- $Id: Main.hs,v 1.7 2003-08-07 12:42:26 raz Exp $
 --
 
 
@@ -50,16 +50,16 @@ bup_one_step r1 rec i =
        return ch_r2
 
 tdown_one_step r1 rec i =
-    do let r2 = RFormula (\a1 -> RFormula (\b1 -> RFormula (\c1 -> 
-		RFormula (\a2 -> RFormula (\b2 -> RFormula (\c2 -> Formula (
-		        (f_apply r1 [a1, b1, c1, a2, b2, c2]) Omega.||
-                         Exists (\a3 -> Exists (\b3 -> Exists (\c3 ->
-                             (f_apply rec [a1, b1, c1, a3, b3, c3]) Omega.&& (f_apply r1 [a3, b3, c3, a2, b2, c2])))) )))))))
-       r2_subset_r1 <- subset (["a", "b", "c"], ["a'", "b'", "c'"], r2) (["a", "b", "c"], ["a'", "b'", "c'"], r1)
+    do let r2 = RFormula (\a1 -> RFormula (\b1 ->  
+		RFormula (\a2 -> RFormula (\b2 -> Formula (
+		        (f_apply r1 [a1, b1, a2, b2]) Omega.||
+                         Exists (\a3 -> Exists (\b3 -> 
+                             (f_apply rec [a1, b1, a3, b3]) Omega.&& (f_apply r1 [a3, b3, a2, b2]))))))))
+       r2_subset_r1 <- subset (["a", "b"], ["a'", "b'"], r2) (["a", "b"], ["a'", "b'"], r1)
        putStr ("r"++ (show i) ++" subset r"++ (show (i - 1)) ++": "++ (show r2_subset_r1) ++ "\n")
-       ch_r2 <- convex_hull (["a", "b", "c"], ["a'", "b'", "c'"], r2)
-       putStr ("ch_r"++ (show i) ++"': " ++ (rformula_print_formula_to_string ch_r2 ["a", "b", "c", "a'", "b'", "c'"]) ++ "\n")
-       ch_r2_subset_r1 <- subset (["a", "b", "c"], ["a'", "b'", "c'"], ch_r2) (["a", "b", "c"], ["a'", "b'", "c'"], r1)
+       ch_r2 <- convex_hull (["a", "b"], ["a'", "b'"], r2)
+       putStr ("ch_r"++ (show i) ++"': " ++ (rformula_print_formula_to_string ch_r2 ["a", "b", "a'", "b'"]) ++ "\n")
+       ch_r2_subset_r1 <- subset (["a", "b"], ["a'", "b'"], ch_r2) (["a", "b"], ["a'", "b'"], r1)
        putStr ("ch_r"++ (show i) ++" subset r"++ (show (i - 1)) ++": "++ (show ch_r2_subset_r1) ++ "\n")
        return ch_r2
 
@@ -80,11 +80,12 @@ main = do
     bup_append_5 <- bup_one_step bup_append_4 append_r 5
 
     putStr "Top-down:\n"
-    tdown_append_1 <- tdown_one_step append_r append_r 1
-    tdown_append_2 <- tdown_one_step tdown_append_1 append_r 2
-    tdown_append_3 <- tdown_one_step tdown_append_2 append_r 3
-    let tdown_append_4 = extract_rformula "{ [a,b,c] -> [a',b',c'] c'+a = a'+c && b = b' && 0 <= c' < c && 0 <= b' && 0 <= a' }"
-    tdown_append_5 <- tdown_one_step tdown_append_4 append_r 5
+    let	append_r' = extract_rformula "{ [a,b] -> [a',b'] a >= 0 && b >= 0 && a' >= 0 && b' >= 0 && a' = a - 1 && b' = b }"
+    tdown_append_1 <- tdown_one_step append_r' append_r' 1
+    tdown_append_2 <- tdown_one_step tdown_append_1 append_r' 2
+    tdown_append_3 <- tdown_one_step tdown_append_2 append_r' 3
+    let tdown_append_4 = extract_rformula "{ [a,b] -> [a',b'] b' >= 0 && b - b' = 0 &&  a' >= 0 && a - 1 >= a' }"
+    tdown_append_5 <- tdown_one_step tdown_append_4 append_r' 5
 
     putStr "[DONE]\n"
 
