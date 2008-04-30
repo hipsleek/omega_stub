ea99959cc0b57dfa5ac50dddc35d80d00ed3d690
popeeaco <unknown>
2008-04-30 11:26:54 +0000

    Link against the publicly available omega files in /usr/local/omega

--- examples.hs/example1/Main.hs
+++ examples.hs/example1/Main.hs
@@ -1,7 +1,7 @@
 ---
 -- This is an example using the new Omega module.
 --
--- $Id: Main.hs,v 1.3 2003-07-28 05:11:02 raz Exp $
+-- $Id: Main.hs,v 1.4 2008-04-30 11:26:54 popeeaco Exp $
 --
 
 -- R1 := { [i] -> [j] : j = i + 1 };
@@ -20,51 +20,70 @@ import Foreign
 import Foreign.C
 import qualified Omega_stub
 import Omega_util
+import Omega_parser
+--main = do
+--    putStr "[START]\n"
+--
+----    let	rf1 = RFormula (\i -> RFormula (\j -> Formula (And [Eq [Coef j (- 1), Coef i 1, Const 1]])))
+----    let	rf2 = RFormula (\i -> RFormula (\j -> Formula (And [Eq [Coef j (- 1), Coef i 1, Const 2]])))
+--
+--    let	rf1 = RFormula (\i -> RFormula (\j -> Formula (And [ j `eq` (i `plus` (1::Int)) ])))
+--    let	rf2 = RFormula (\i -> RFormula (\j -> Formula (And [ j `eq` (i `plus` (2::Int)) ])))
+--
+--
+--    (ptr_r1, rf1) <- build_relation (["x"], ["y"], rf1)
+--    (ptr_r2, rf2) <- build_relation (["x"], ["y"], rf2)
+--
+--    print (show rf1)
+--    print (show rf2)
+--
+--    eval_relation ptr_r1 rf1
+--    eval_relation ptr_r2 rf2
+--
+--    Omega_stub.relation_print ptr_r1
+--    Omega_stub.relation_print ptr_r2
+--    ptr_r <- Omega_stub.composition ptr_r1 ptr_r2
+--    Omega_stub.relation_print ptr_r
+--
+----    r_ptr_string <- Omega_stub.relation_print_with_subs_to_string r False
+----    r_string <- peekCString r_ptr_string
+----    print r_string
+--    Omega_stub.relation_print_with_subs ptr_r False
+--    Omega_stub.relation_print ptr_r
+--
+--
+--    putStr "extract testing...\n"
+--    let	rf3 = RFormula (\i -> RFormula (\j -> RFormula (\k -> Formula (And [ j `eq` (i `plus` k) ] ))))
+----    let	rf3 = RFormula (\i -> RFormula (\j ->  Formula (And [ j `eq` (i `plus` (2::Int))) ] )))
+----    let	rf3 = RFormula (\i -> RFormula (\j -> Formula (And [ Exists (\k -> And [ j `eq` (i `plus` k `plus` (2::Int)) ] ) ] )))
+--    (ptr_r3, rf3) <- build_relation (["a", "b", "c"], [], rf3)
+--    putStr ((show rf3) ++ "\n")
+--    eval_relation ptr_r3 rf3
+--    Omega_stub.relation_finalize ptr_r3
+--    Omega_stub.relation_print ptr_r3
+--    Omega_stub.query_experiment ptr_r3
+--
+----    rf3' <- extract_formula_from_set ptr_r3
+----    putStr ((show rf3') ++ "\n")
+--
+--    putStr "[DONE]\n"
+
+
 
 main = do
     putStr "[START]\n"
 
---    let	rf1 = RFormula (\i -> RFormula (\j -> Formula (And [Eq [Coef j (- 1), Coef i 1, Const 1]])))
---    let	rf2 = RFormula (\i -> RFormula (\j -> Formula (And [Eq [Coef j (- 1), Coef i 1, Const 2]])))
-
-    let	rf1 = RFormula (\i -> RFormula (\j -> Formula (And [ j `eq` (i `plus` (1::Int)) ])))
-    let	rf2 = RFormula (\i -> RFormula (\j -> Formula (And [ j `eq` (i `plus` (2::Int)) ])))
+    let rf3 = extract_rformula "{[x,y]: 1 <= x && x=y}"
+    (ptr_r3, rf3) <- build_relation (["x"], ["y"], rf3)
 
 
-    (ptr_r1, rf1) <- build_relation (["x"], ["y"], rf1)
-    (ptr_r2, rf2) <- build_relation (["x"], ["y"], rf2)
-
-    print (show rf1)
-    print (show rf2)
-
-    eval_relation ptr_r1 rf1
-    eval_relation ptr_r2 rf2
-
-    Omega_stub.relation_print ptr_r1
-    Omega_stub.relation_print ptr_r2
-    ptr_r <- Omega_stub.composition ptr_r1 ptr_r2
-    Omega_stub.relation_print ptr_r
-
---    r_ptr_string <- Omega_stub.relation_print_with_subs_to_string r False
---    r_string <- peekCString r_ptr_string
---    print r_string
-    Omega_stub.relation_print_with_subs ptr_r False
-    Omega_stub.relation_print ptr_r
-
-
-    putStr "extract testing...\n"
-    let	rf3 = RFormula (\i -> RFormula (\j -> RFormula (\k -> Formula (And [ j `eq` (i `plus` k) ] ))))
---    let	rf3 = RFormula (\i -> RFormula (\j ->  Formula (And [ j `eq` (i `plus` (2::Int))) ] )))
---    let	rf3 = RFormula (\i -> RFormula (\j -> Formula (And [ Exists (\k -> And [ j `eq` (i `plus` k `plus` (2::Int)) ] ) ] )))
-    (ptr_r3, rf3) <- build_relation (["a", "b", "c"], [], rf3)
-    putStr ((show rf3) ++ "\n")
     eval_relation ptr_r3 rf3
-    Omega_stub.relation_finalize ptr_r3
     Omega_stub.relation_print ptr_r3
-    Omega_stub.query_experiment ptr_r3
+    Omega_stub.relation_print_with_subs ptr_r3 False
 
---    rf3' <- extract_formula_from_set ptr_r3
---    putStr ((show rf3') ++ "\n")
+    r <- Omega_stub.relation_print_formula_to_string ptr_r3
+    r_string <- peekCString r
+    print r_string
 
     putStr "[DONE]\n"
 
